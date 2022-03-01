8bf63623d12c4378001366c730948c2bc320085a
haavardsr <79541783+haavardsr@users.noreply.github.com>
2022-03-01 13:28:15 +0100

    Update SuperMarket.java

--- src/supermarket/SuperMarket.java
+++ src/supermarket/SuperMarket.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package supermarket;
-
+import eventsim.Constants;
 import eventsim.Event;
 import eventsim.EventSim;
 import java.util.ArrayList;
@@ -17,32 +17,47 @@ import java.util.List;
  */
 public class SuperMarket {
 
-    public static void main(String[] arts) {
+    public static void main(String[] args) {
         SuperMarket supern = new SuperMarket();
         supern.startSim();
     }
 
-    public static final int NUM_CHECKOUTS = 1;
-    public static final int NUM_CUSTOMERS = 4;
+    private int numOfCheckouts = Constants.SM_NUM_CHECKOUTS;
+    private int numOfCustomers = Constants.SM_NUM_CUSTOMERS;
 
     Checkout[] checkouts;
     List<Customer> customers;
     List<Event> init;
 
-
     public SuperMarket() {
-        checkouts = new Checkout[NUM_CHECKOUTS];
-        for (int i = 0; i < NUM_CHECKOUTS; i++)
+
+        checkouts = new Checkout[numOfCheckouts];
+        for (int i = 0; i < numOfCheckouts; i++) {
             checkouts[i] = new Checkout(this, i);
+        }
+
         customers = new ArrayList<>();
         init = new ArrayList<Event>();
-        for (int i = 0; i < NUM_CUSTOMERS; i++) {
+
+        // Tester en kunde som ikke handler noen ting
+        /*
+        Customer test = new Customer(this, 600);
+        test.changeNumOfProdcuts(0);
+        init.add(new BeginShoppingEvent(test));
+        customers.add(test);
+        */
+
+        for (int i = 0; i < numOfCustomers; i++) {
             Customer c = new Customer(this, i);
             init.add(new BeginShoppingEvent(c));
             customers.add(c);
         }
+
     }
 
+    public Checkout[] getCheckouts() {
+        return checkouts;
+    }
 
     public void startSim() {
         EventSim sim = EventSim.getInstance();
